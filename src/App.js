b67944d1714c40cb5373a4f61c9e11b95863318c
saramagdyy <126898600+saramagdyy@users.noreply.github.com>
2024-09-23 12:23:08 +0300

    rout

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ const App = () => {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="e-commerce/" element={<Home />} />
-            <Route path="e-commerce//login" element={<Login />} />
-            <Route path="e-commerce//register" element={<Register />} />
-            <Route path="e-commerce//order-history" element={<OrderHistory />} />
-            <Route path="e-commerce//cart" element={<Cart />} />
-            <Route path="e-commerce//products/:id" element={<ProductDetail />} />
-            <Route path="e-commerce//checkout" element={<CheckoutPage />} />
-            <Route path="e-commerce//order-confirmation" element={<OrderConfirmation />} />
+            <Route path="/e-commerce/" element={<Home />} />
+            <Route path="/e-commerce/login" element={<Login />} />
+            <Route path="/e-commerce/register" element={<Register />} />
+            <Route path="/e-commerce/order-history" element={<OrderHistory />} />
+            <Route path="/e-commerce/cart" element={<Cart />} />
+            <Route path="/e-commerce/products/:id" element={<ProductDetail />} />
+            <Route path="/e-commerce/checkout" element={<CheckoutPage />} />
+            <Route path="/e-commerce/order-confirmation" element={<OrderConfirmation />} />
           </Routes>
         </Router>
       </CartProvider>
